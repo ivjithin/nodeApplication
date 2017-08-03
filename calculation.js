1ce6f229d1f3c6a540eddad8f7eaa22e51acea81
Jithin <jithin.iv@tcs.com>
2017-08-03 09:59:07 +0000

    files changes

--- calculation.js
+++ calculation.js
@@ -1,16 +1,30 @@
 class Calculation{
  
-  constructor() {
 
+  areaWithCallback(rectangle,callBack) {
+    if(!!rectangle){
+      callBack(null,rectangle.width * rectangle.height);
+    }else{
+      callBack(new Error("Object is undefined"));
+    }
+  
   }
-
-  area(rectangle) {
+  area(rectangle){
     return rectangle.width * rectangle.height;
   }
 
   circumference(rectangle) {
-    return 2*(rectangle.width +  rectangle.height);
+    try{
+          if(!!rectangle){
+              return  2*(rectangle.width +  rectangle.height);
+          }else{
+            return 0;
+          }
+    }catch(err){
+      console.log(err);
+      return 0;
+    }
+   
   }
-
 }
 module.exports = Calculation;
--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 let Calculation = require('./calculation.js');
 let Rectangle = require('./rectangle.js');
 
-exports.handler = (event, context, callback) => {
+//exports.handler = (event, context, callback) => {
+//	process.on('uncaughtException', function (err) {
+  //  console.log( "UNCAUGHT EXCEPTION " );
+   
+	//});
 	console.log("Start...");
 	let calc = new Calculation();
 	let rectangle = new Rectangle(10,5);
-	let area = calc.area(rectangle);
+	let area = 0;
+	calc.areaWithCallback(rectangle,function(err,data){
+		if(err){
+			console.log("error");
+			area = 0;
+		}else{
+			area = data;
+		}
+	});
 	console.log("Area of Rectangle is",area);
 	let circum = calc.circumference(rectangle);
 	console.log("Circumference  of Rectangle is",circum);
 
-};
+//};
